d4857588fe486afd0d2406c87c5f40c845531db1
snehadhande <snehadhande@snehas-air.lan>
2023-04-04 13:59:56 -0500

    Title test commit

---
+++ src/test/java/tests/GoogleTitleTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GoogleTitleTest {
+
+	WebDriver driver;
+	@BeforeTest
+	public void setUp() 
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/snehadhande/Downloads/Software Download/chromedriver_mac_arm64/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		
+	}
+	
+	@Test
+	public void titleTest() 
+	{
+		Assert.assertEquals(driver.getTitle(), "Google"); 
+	}
+	
+	
+	@AfterTest
+	public void teardown() {
+		driver.quit();
+	}
+}
