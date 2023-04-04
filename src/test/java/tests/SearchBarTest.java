fb5f4455c55685aa2b9b1add5c8033fddd137551
snehadhande <snehadhande@snehas-air.lan>
2023-04-04 14:22:56 -0500

    Search Bar Test commit

---
+++ src/test/java/tests/SearchBarTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SearchBarTest {
+
+	WebDriver driver;
+	@BeforeTest
+	public void setUp() 
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/snehadhande/Downloads/Software Download/chromedriver_mac_arm64/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		
+	}
+	
+	@Test
+	public void titleTest() 
+	{
+		Assert.assertTrue(driver.findElement(By.name("q")).isDisplayed());  
+	}
+	
+	
+	@AfterTest
+	public void teardown() {
+		driver.quit();
+	}
+}
